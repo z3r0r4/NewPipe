d04771858686df381a95207ff8ba13e22d3a8599
z3r0r4 <R4C1.meru@protonmail.com>
2021-09-29 16:12:45 +0200

    added compile script that replcaes api with hack api

--- compileWithHiddenAndroid.sh
+++ compileWithHiddenAndroid.sh
@@ -6,9 +6,9 @@ mv $sdk/android.jar $sdk/android.jar.old #rename originalapi to unused
 mv $sdk/android-30fixed.jar $sdk/android.jar #rename hackapi to used
 echo "done replacing original!"
 echo "execute gradle build and install"
-./gradlew installDebug #run gradle build and install
-echo "removing hack api from sdk folder and placing the original"
-mv $sdk/android.jar $sdk/android.jar.hack #rename used hackapi to unused
-mv $sdk/android.jar.old $sdk/android.jar #rename unused originalapi to used
-rm $sdk/android.jar.hack
-echo "done replacing hack!" #check size to prove if you want original is ~20mb hack is ~28mb
+#./gradlew installDebug #run gradle build and install
+#echo "removing hack api from sdk folder and placing the original"
+#mv $sdk/android.jar $sdk/android.jar.hack #rename used hackapi to unused
+#mv $sdk/android.jar.old $sdk/android.jar #rename unused originalapi to used
+#rm $sdk/android.jar.hack
+#echo "done replacing hack!" #check size to prove if you want original is ~20mb hack is ~28mb
