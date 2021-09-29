b675308130e1cd1de6361439e494e3a5219fe456
z3r0r4 <R4C1.meru@protonmail.com>
2021-09-29 17:59:18 +0200

    improved auto release

--- compileWithHiddenAndroid.sh
+++ compileWithHiddenAndroid.sh
@@ -6,14 +6,42 @@ cp ./app/libs/android-30fixed.jar $sdk #copy hack to sdk location
 mv $sdk/android.jar $sdk/android.jar.old #rename originalapi to unused
 mv $sdk/android-30fixed.jar $sdk/android.jar #rename hackapi to used
 echo "done replacing original!"
-read -p "execute gradle build and install" </dev/tty
-./gradlew installDebug #run gradle build and install
-gh release create hiddenApi-v$date
-read -p "uploading generated apk" </dev/tty
-gh release upload hiddenApi-v$date ./app/build/outputs/apk/debug/app-debug.apk
-read -p "removing hack api from sdk folder and placing the original" </dev/tty
+
+
+read -p "build gradle and install ( /n)" -n 1 -r
+echo    
+if [[ $REPLY =~ ^[Nn]$ ]]
+then
+	echo "skipping"
+else
+	./gradlew installDebug #run gradle build and install
+fi
+
+
+read -p "create release ( /n)" -n 1 -r
+echo    
+if [[ $REPLY =~ ^[Nn]$ ]]
+then
+	echo "skipping"
+else
+	gh release create hiddenApi-v$date
+fi
+#gh release delete hiddenApi-vTest
+
+
+read -p "upload generated apk ( /n)" -n 1 -r
+echo    
+if [[ $REPLY =~ ^[Nn]$ ]]
+then
+	echo "skipping"
+else
+	gh release upload hiddenApi-v$date ./app/build/outputs/apk/debug/app-debug.apk
+fi
+
+
+read -p "removing hack api from sdk folder and placing the original (Enter)" </dev/tty
 mv $sdk/android.jar $sdk/android.jar.hack #rename used hackapi to unused
 mv $sdk/android.jar.old $sdk/android.jar #rename unused originalapi to used
 rm $sdk/android.jar.hack
 echo "done replacing hack!" #check size to prove if you want original is ~20mb hack is ~28mb
-read -p "Press Enter to continue" </dev/tty
+read -p "Press Enter to close" </dev/tty
